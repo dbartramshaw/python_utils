27ca9ebffa524fa8d6b42cdf880c83ba5955cee9
Bartram-Shaw, David <david.bartram-shaw@outlook.com>
2018-02-03 23:05:18 +0000

    update utils

--- numpy_utils.py
+++ numpy_utils.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+
+
+############################
+# package utils
+############################
 import numpy as np
+from __future__ import division
+
 
 
 def join_two_dicts(a,
@@ -28,3 +36,57 @@ def join_two_dicts(a,
             for key, value in d.iteritems():
                 dd[key].append(value)
         return dd
+
+
+
+############################
+# numpy notes
+############################
+
+#reshape to give 1 column
+vec.reshape(1,100).shape # = (1,100)
+
+# Reshape to
+feature.shape #(286,) #286 rows
+feature = feature.reshape(X.shape[0], 1)
+feature.shape #(286, 1)
+
+#append
+np.append(data,new_col)
+
+#argsort
+x = numpy.array([1.48,1.41,0.0,0.1])
+a = x.argsort()
+print x[a] #, we will get array([ 0. , 0.1 , 1.41, 1.48]
+
+
+# setdiff - difference in two arrays
+np.setdiff1d(a, b)
+
+# count non zero
+tess_nums = [0,0,0,1]
+np.count_nonzero(tess_nums)
+
+
+
+############################
+# Matrices
+############################
+#Dense column wise
+full_dense = np.hstack((word_dense,likes_dense))
+
+#Dense row wise
+full_dense = np.vstack((word_dense,likes_dense))
+
+import scipy
+full_sparse = scipy.sparse.hstack((word_features,likes_matrix))
+full_sparse.shape
+
+
+
+############################
+# List Comprehensions
+############################
+[a if a else 2 for a in [0,1,0,3]]
+
+flat_list = [item for sublist in l for item in sublist]
