9c3144321d86e77e2d9e1909dc138c1ab8d44d71
Bartram-Shaw, David <david.bartram-shaw@outlook.com>
2018-03-02 13:57:25 +0000

    opencv installation

---
+++ ec2_env_setup_script.sh
@@ -0,0 +1,99 @@
+# -y added to default yes
+
+
+
+
+sudo apt-get update
+sudo apt-get upgrade
+Y
+# Install the required developer tools and packages:
+sudo apt-get install -y build-essential cmake pkg-config
+
+# These packages allow you to load various image file formats such as JPEG, PNG, TIFF, etc.
+sudo apt-get install -y libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
+
+# Install the GTK development library. This library is used to build Graphical User Interfaces (GUIs)
+sudo apt-get install -y libgtk2.0-dev
+
+# not neccesary if not computer vision is going to happen
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libatlas-base-dev gfortran
+
+# I normally like the boost library to be installed.
+# This will already install python 2.7
+# If one does not want boost simply replace
+# next two lines with
+# sudo apt-get install python
+sudo apt-get install -y libboost-all-dev
+
+# get pip. This can also be done with:
+# sudo apt-get install python-pip
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo apt-get install -y python-tk
+
+
+
+# install python things
+sudo pip install --upgrade pip
+sudo pip install numpy
+sudo pip install scipy
+sudo pip install scikit-learn
+sudo pip install scikit-optimize
+sudo pip install pandas
+sudo pip install pandasql
+sudo pip install matplotlib
+sudo pip install awscli
+sudo pip install jupyter
+sudo pip install seaborn
+sudo pip install python-dotenv
+sudo pip install joblib
+sudo pip install gensim
+sudo pip install flask
+sudo pip install autopep8
+sudo pip install flake8
+sudo pip install nose
+sudo pip install Cython
+sudo pip install xgboost
+sudo pip install boto
+sudo pip install boto3
+sudo pip install feather-format
+sudo pip install scikit-optimize
+sudo pip install hyperopt
+sudo pip install h5py
+sudo pip install hyperopt
+sudo pip install ipython
+sudo pip install joblib
+sudo pip install jupyter
+sudo pip install Keras
+sudo pip install lightfm
+pip install git+https://github.com/coreylynch/pyFM
+sudo pip install pymc
+sudo pip install pymc3
+sudo pip install requests
+sudo pip install sklearn-pandas
+sudo pip install theano
+sudo pip install tensorflow
+sudo apt install spark
+
+sudo apt-get install -y tcl-dev tk-dev python-tk python3-tk
+pip uninstall matplotlib
+git clone https://github.com/matplotlib/matplotlib.git
+cd matplotlib
+#python setup.py install
+
+
+# Install Java
+# https://www.digitalocean.com/community/tutorials/how-to-install-java-on-ubuntu-12-04-with-apt-get
+sudo apt-get update
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+java -version
+
+
+
+# aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t1.micro --key-name MyKeyPair --security-groups my-sg
+# aws ec2 run-instances --image-id ami-5ede8338 --count 1 --instance-type t2.micro --key-name ~/keys/dbs-test.pem
+
+# chmod 400 /Users/davidbartram-shaw/keys/dm-dev.pem
+# chmod 400 /Users/davidbartram-shaw/keys/dbs-test.pem
--- ec2_utils.sh
+++ ec2_utils.sh
@@ -81,6 +81,134 @@ dot -V
 sudo apt install graphviz
 
 
+#####################
+# Install OPENCV 
+# UBUNTU 
+#####################
+# from the guru https://www.pyimagesearch.com/2016/10/24/ubuntu-16-04-how-to-install-opencv/
+
+''' Step #1: Install OpenCV dependencies on Ubuntu 16.04 '''
+sudo apt-get update
+sudo apt-get upgrade
+# install developer tools:
+sudo apt-get install build-essential cmake pkg-config
+# facilitate the loading and decoding process for file formats from disk such as JPEG, PNG, TIFF
+sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
+# install packages used to process video streams and access frames from cameras
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+# highgui module (GUI for cv2.imshow ect) relies on the GTK library
+sudo apt-get install libgtk-3-dev
+# libraries that are used to optimize various functionalities inside OpenCV, such as matrix operations:
+sudo apt-get install libatlas-base-dev gfortran
+#  installing the Python development headers and libraries for both Python 2.7 and Python 3.5
+sudo apt-get install python2.7-dev python3.5-dev
+
+''' Step #2: Download the OpenCV source '''
+cd ~
+# most recent version of OpenCV is 3.1.0 , which we download a .zip  of and unarchive
+# found here https://github.com/opencv/opencv
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip
+sudo apt install unzip
+unzip opencv.zip
+# we also need the opencv_contrib repository as well:
+# contains SIFT and SURF on OpenCV 3+ 
+wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip
+unzip opencv_contrib.zip
+
+
+'''Step #3: Setup your Python environment — Python 2.7 or Python 3 '''
+# If setting up virtual environments
+pip install numpy
+
+'''Step #4: Configuring and compiling OpenCV on Ubuntu 16.04'''
+cd ~/opencv-3.1.0/
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D INSTALL_C_EXAMPLES=OFF \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.1.0/modules \
+    -D BUILD_EXAMPLES=ON ..
+
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D INSTALL_C_EXAMPLES=OFF \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D BUILD_EXAMPLES=ON ..
+
+
+#####################
+# Install OPENCV 
+# UBUNTU - Working
+#####################
+# https://www.learnopencv.com/install-opencv3-on-ubuntu/
+sudo apt-get remove x264 libx264-dev #remove old versions
+sudo apt-get install build-essential checkinstall cmake pkg-config yasm
+sudo apt-get install git gfortran
+sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev
+ 
+# sudo apt-get install libtiff4-dev  # If you are using Ubuntu 14.04
+sudo apt-get install libtiff5-dev  # If you are using Ubuntu 16.04
+
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt-get install libxine2-dev libv4l-dev
+sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt-get install libvorbis-dev libxvidcore-dev
+sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get install x264 v4l-utils
+ 
+# Optional dependencies
+sudo apt-get install libprotobuf-dev protobuf-compiler
+sudo apt-get install libgoogle-glog-dev libgflags-dev
+sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo -H pip2 install -U pip numpy
+sudo -H pip3 install -U pip numpy
+pip install numpy scipy matplotlib scikit-image scikit-learn ipython
+
+git clone https://github.com/opencv/opencv.git
+cd opencv 
+git checkout 3.3.1 
+
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout 3.3.1
+cd ..
+
+cd opencv 
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D INSTALL_C_EXAMPLES=ON \
+      -D INSTALL_PYTHON_EXAMPLES=ON \
+      -D WITH_TBB=ON \
+      -D WITH_V4L=ON \
+      -D WITH_QT=ON \
+      -D WITH_OPENGL=ON \
+      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+      -D BUILD_EXAMPLES=ON ..
+
+
+
+
+''' Step 5.3: Compile and Install '''
+# find out number of CPU cores in your machine
+nproc
+# substitute 4 by output of nproc
+make -j1
+sudo make install
+sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+
 
 
 """ #-----------------------------------------------------------------------------------------------------------
